c6198c55eca888ab90427fbf94470fd56a8d6e71
junaid <junaid1460@gmail.com>
2017-10-10 21:07:42 +0530

    Ignored some javascript files in gitattr

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
-# from __future__ import print_functiontry
-from 
+import http.server as server
+import socketserver as socket
+from sys import argv
+port = None
 try:
-    x = 2/0
-except BaseException:
-    print "cann"
-
-
-
-class myClass(object):
-    def __init__(self, x):
-        self.myval = x
-    def __
+    port = int(argv[1])
+except:
+    port = 9999
+class Handler(server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # print(sir(self.request))
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"get")
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"post")
+try:
+    socket.TCPServer(('', port), Handler).serve_forever()
+except:
+    pass
